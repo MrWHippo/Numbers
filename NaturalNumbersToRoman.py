9068b74b9f828b9adfb164c8b137c06d943ccff2
Jonty <17radcj@olab.org.uk>
2023-01-10 17:46:38 +0000

    Finished Natural to Roman

--- NaturalNumbersToRoman.py
+++ NaturalNumbersToRoman.py
@@ -1,11 +1,19 @@
+import time
 InNum = int(input("Enter Number to convert to roman numerials: "))
 
-RomanNums = {1: "I",4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",400: "CD", 500: "D", 900: "CM", 1000: "M"}
+RomanNums = {1: "I",4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C",400: "CD", 500: "D", 900: "CM", 1000: "M", 4000: "MV", 5000: "V"}
 
 left = True
 answer = []
 
-while left:
-    if InNum 
+time1 = time.perf_counter()
+for x in reversed(RomanNums):
+    while (InNum - x) >= 0:
+        answer.append(RomanNums[x])
+        InNum -= x
+time2 = time.perf_counter()
 
+timetook = time2 - time1
 
+print("".join(answer))
+print(timetook, "Seconds")
